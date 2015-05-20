d59fccf3f9af7cefe9a9d4b8da831355fab9620c
tclewis91 <tclewis91@gmail.com>
2015-05-20 17:56:22 -0500

    intro draft

---
+++ guessing_game.rb
@@ -0,0 +1,10 @@
+print "Hey, welcome to the guessing game! What's your name?"
+name = gets.upcase.chomp
+puts "Cool, so #{name}, do you wanna play? (y/n)"
+  if ans == 'y'
+    puts "hell yeah!"
+  elsif ans == 'n'
+    puts "later!"
+  else ans == ''
+    puts "sorry,please re-type that"
+  end
