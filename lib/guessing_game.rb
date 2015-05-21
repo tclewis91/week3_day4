cf0b53f3d7b07d147a114c44fa183123fae584e2
tclewis91 <tclewis91@gmail.com>
2015-05-21 08:49:05 -0500

    corrected w directions

--- lib/guessing_game.rb
+++ lib/guessing_game.rb
@@ -5,10 +5,10 @@ class Guessing
   end
 #text intro to game
   def intro
-  print "Hey, welcome to the guessing game! What's your name?"
-  name = gets.upcase.chomp
-  puts "Cool, so #{name}, do you wanna play? (y/n)"
-  ans = gets.chomp
+    print "Hey, welcome to the guessing game! What's your name?"
+    name = gets.upcase.chomp
+    puts "Cool, so #{name}, do you wanna play? (y/n)"
+    ans = gets.chomp
     if ans == 'y'
       puts "hell yeah!"
     elsif ans == 'n'
@@ -23,10 +23,24 @@ end
     dice = [1..100].rand
   end
 
-   def game
-      print "Do you feel lucky punk? Fine. Pick a number"
-      guess = get.chomp.to_i
-      @counter += 1
+  def game_setup
+    print "Do you feel lucky punk? Fine. Pick a number"
+    guess = get.chomp.to_i
+    @counter += 1
+  end
+
+  def game_internals
+    until @counter == 5
+      if guess == dice
+        puts "Congrats"
+      break
+    elsif guess != dice && guess >= dice
+        puts "You're too high bro"
+    elsif guess != dice && guess <= dice
+        puts "Hmmm too low"
     end
+  end
+end
+
 
 end
