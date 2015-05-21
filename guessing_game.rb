aba8faf76e900b30a7803767f863b9b3c653f8b6
tclewis91 <tclewis91@gmail.com>
2015-05-20 22:35:13 -0500

    comments for intro

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,12 +1,16 @@
 class Guessing_game
-print "Hey, welcome to the guessing game! What's your name?"
-name = gets.upcase.chomp
-puts "Cool, so #{name}, do you wanna play? (y/n)"
-ans=gets.chomp
-  if ans == 'y'
-    puts "hell yeah!"
-  elsif ans == 'n'
-    puts "later!"
-  else ans == ''
-    puts "sorry,please re-type that"
+  #intro
+  def intro
+  print "Hey, welcome to the guessing game! What's your name?"
+  name = gets.upcase.chomp
+  puts "Cool, so #{name}, do you wanna play? (y/n)"
+  ans=gets.chomp
+    if ans == 'y'
+      puts "hell yeah!"
+    elsif ans == 'n'
+      puts "later!"
+    else ans == ''
+      puts "sorry,please re-type that"
   end
+#should that have been more than one method?
+
