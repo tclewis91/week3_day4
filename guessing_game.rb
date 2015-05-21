8802a23c228196419a097d3dd7e78e34b86be894
tclewis91 <tclewis91@gmail.com>
2015-05-20 22:31:01 -0500

    class

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,6 +1,8 @@
+class Guessing_game
 print "Hey, welcome to the guessing game! What's your name?"
 name = gets.upcase.chomp
 puts "Cool, so #{name}, do you wanna play? (y/n)"
+ans=gets.chomp
   if ans == 'y'
     puts "hell yeah!"
   elsif ans == 'n'
